3e9419a4281c437c99428b207790bbd3a43b7610
raman <ramangalive1234@gmail.com>
2026-09-19 21:47:36 +0530

    Time: 6 ms (12.74%) | Memory: 10.7 MB (20.55%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        
+        if (num1 == "0" || num2 == "0")
+            return "0";
+
+        int n = num1.size();
+        int m = num2.size();
+
+        vector<int> result(n + m, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                
+                int a = num1[i] - '0';
+                int b = num2[j] - '0';
+
+                int product = a * b;
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = product + result[p2];
+
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+
+        string ans = "";
+
+        for (int x : result) {
+            if (ans.empty() && x == 0)
+                continue;
+
+            ans += to_string(x);
+        }
+
+        return ans;
+    }
+};
