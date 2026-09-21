4288ed211c8ac60e715d68e2ae1723d0429e0361
raman <ramangalive1234@gmail.com>
2026-09-21 18:56:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.49%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
-    long long countCommas(long long n) {
-        long long ans = 0;
-        long long start = 1000;
-        long long commas = 1;
+    int countCommas(int n) {
+        if (n < 1000)
+            return 0;
 
-        while (start <= n) {
-            long long end = start * 1000 - 1;
-
-            long long count = min(n, end) - start + 1;
-
-            ans += count * commas;
-
-            start *= 1000;
-            commas++;
-        }
-
-        return ans;
+        return n - 999;
     }
 };
